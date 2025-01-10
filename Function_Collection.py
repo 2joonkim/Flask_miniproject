e4c82667c83c501dc902dd8025decdd19d6042c3
2joonkim <joon271694@gmail.com>
2025-01-10 15:56:46 +0900

    📝 Answer/image조회,생성 함수 제작 완료
    제작 완료

--- Function_Collection.py
+++ Function_Collection.py
@@ -1,7 +1,25 @@
 #QUESTION
 def get_question(question_id):
     question = Question.query.get(question_id)
-return question
+    return question
 
 #ANSWER
-    
+def get_answer(answer_id):
+    answer = Answer.query.get(answer_id)
+    return answer
+
+#image 조회
+def get_image(image_id):
+    image = Image.query.get(image_id)
+    return image
+
+#image 생성
+def create_image(url, type, question_id):
+    image = Image(
+        url=url,
+        type=type,
+        question_id=question_id
+    )
+    db.session.add(image)
+    db.session.commit()
+    return image
