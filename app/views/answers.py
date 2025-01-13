a06d00cc8083d0d67b8adb4f77d4ec1d64d72420
2joonkim <joon271694@gmail.com>
2025-01-13 15:47:31 +0900

    ✅ Answers/images API 업데이트
    업데이트 진행 완료했습니다.

---
+++ app/views/answers.py
@@ -0,0 +1,27 @@
+from flask import Blueprint, request, jsonify
+from app import db
+from app.models import User, Choices, Question  # Question 모델 임포트
+
+# Blueprint 생성
+answers_bp = Blueprint('answers', __name__)
+
+@answers_bp.route('/questions/<int:question_id>/choices', methods=['POST'])
+def create_choice(question_id):
+    data = request.get_json()
+
+    # 질문을 찾기 + 질문이 없다면 404 오류 노출
+    question = Question.query.get_or_404(question_id)
+
+    # 선택지 생성
+    # sqe는 순서나 다른 관련정보를 나타내는 값입니다. ex. 질문1 / 질문2 등
+    choice = Choices(
+        content=data['content'],
+        sqe=data['sqe'],
+        question_id=question.id
+    )
+
+    # 데이터베이스 추가
+    db.session.add(choice)
+    db.session.commit()
+
+    return jsonify(choice.to_dict()), 201
---
+++ app/views/choices.py
@@ -0,0 +1,26 @@
+from flask import Blueprint, request, jsonify
+from app import db
+from app.models import Question, Choices
+
+#블루프린트 설정
+choices_bp = Blueprint('choices', __name__)
+
+@choices_bp.route('/questions/<int:question_id>/choices', methods=['POST'])
+def create_choice(question_id):
+    data = request.get_json()
+
+    # 질문을 찾기 + 질문이 없다면 404 오류 노출
+    question = Question.query.get_or_404(question_id)
+
+    # 선택지 생성
+    choice = Choices(
+        content=data['content'],
+        sqe=data['sqe'],
+        question_id=question.id
+    )
+
+    # 데이터베이스 추가
+    db.session.add(choice)
+    db.session.commit()
+
+    return jsonify(choice.to_dict()), 201
