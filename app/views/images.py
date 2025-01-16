7210e0b01adab32a2b618bffd3a7dd3d39233e22
Yan02251 <xcvbnmz1238@gmail.com>
2025-01-16 17:02:17 +0900

    ✅ Json 이미지 추가

--- app/views/images.py
+++ app/views/images.py
@@ -1,28 +1,46 @@
-from flask import Blueprint, jsonify, request
-from app.models import Image
-from config import db
-
-# Blueprint 생성
-images_bp = Blueprint("images", __name__, url_prefix="/images")
-
-# 이미지 목록 조회
-@images_bp.route("/", methods=["GET"])
-def get_images():
-    images = Image.query.all()
-    return jsonify([img.to_dict() for img in images]), 200
-
-# 이미지 업로드
-@images_bp.route("/", methods=["POST"])
-def upload_image():
-    data = request.get_json()
-    url = data.get("url")
-    type = data.get("type")
-
-    if not url or not type:
-        return jsonify({"error": "필수 데이터가 부족합니다."}), 400
-
-    new_image = Image(url=url, type=type)
-    db.session.add(new_image)
-    db.session.commit()
-
-    return jsonify(new_image.to_dict()), 201
+{
+  "images": [
+    {
+      "id": "1",
+      "url": "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyNDEyMDRfMyAg%2FMDAxNzMzMjg5ODA5OTY5.gGL0Rk7d0IBQyGpyY-6CWMtN708PuVoDjJplR7yOUvEg.dpFul6aHcoOAvMzpg2g3Rn8v4H2zO_KtHm2A4M8-PYYg.PNG%2Fimage.png&type=sc960_832",
+      "width": 1329,
+      "height": 611,
+      "alt": "간판 이미지"
+    },
+    {
+      "id": "2",
+      "url": "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyNDEyMjdfNDYg%2FMDAxNzM1Mjc3NzExMDMw.v8I-Iq_4fCSz9ktXBnIlWUhiJTCx4qTVVZDqTJmYMWUg.ZWeRAwllEHloi8hdiSGhlYMGKHzQLAfcgWrKZXxoxbsg.PNG%2F%C8%E6%B9%E9%BF%E4%B8%AE%BB%E7.png&type=sc960_832",
+      "width": 761,
+      "height": 785,
+      "alt": "1번 이미지"
+    },
+    {
+      "id": "3",
+      "url": "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyNDEwMTZfMjg2%2FMDAxNzI5MDUzMjgzNjUx.9-NW9r01-IxWPWrVqbQQeK3_5EqGOS_w6Fl86WXnRkog.hVZhj8lG-OlOrrMvwM8nFCW3OJvWp9hnbkVSvVUIOqgg.JPEG%2FScreenshot%A3%DF20241016%A3%DF114519%A3%DFVideo_Player.jpg&type=sc960_832",
+      "width": 900,
+      "height": 405,
+      "alt": "2번 이미지"
+    },
+    {
+      "id": "4",
+      "url": "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyNDEwMDJfMjEg%2FMDAxNzI3ODc2NzUyMzcw.j6-PwruPcKRwtU2LDVNSQQv4druMZnOWmA_Z_bgQIIog.6ituSX68n-CLe2PLv4GwywYdpjZjXaaip1v6Ppf7yfYg.PNG%2Fimage.png&type=sc960_832",
+      "width": 766,
+      "height": 398,
+      "alt": "3번 이미지"
+    },
+    {
+      "id": "5",
+      "url": "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyNDEwMTVfMTAg%2FMDAxNzI4OTQ5NDY3NzIy.A-otFR2IS-aoUBwgWP0pOB4VeuAoXBNxu3OFf1HnzWQg.Sf1iU-N3YBXELBrZHxttj1fxLmiL1Bm-DLRwd4dvajAg.JPEG%2FUntitled-1.jpg&type=sc960_832",
+      "width": 1080,
+      "height": 1080,
+      "alt": "4번 이미지"
+    },
+    {
+      "id": "6",
+      "url": "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyNDEwMDlfNTkg%2FMDAxNzI4NDQ4OTM4NDk3.-TNmttUdoWgFVkdr8T2M4VL0nH1lXVbrT_agmn-W1SAg.BuiRean08PztkzxOvsOQiv0AFVwedB8kHJ4q9-3CO8Ig.JPEG%2F1728448940567.jpg&type=sc960_832",
+      "width": 1037,
+      "height": 1232,
+      "alt": "5번 이미지"
+    }
+  ]
+}
